b4fb03dc926d1c446e2df37eb613582663bd60a5
Divakar <divakar39312@gmail.com>
2022-08-09 21:57:32 +0530

    [Divakar]:UC6 added

--- src/com/Bridgelabz/EmpWage.java
+++ src/com/Bridgelabz/EmpWage.java
@@ -3,34 +3,37 @@ package com.Bridgelabz;
 public class EmpWage {
 	public static final int isparttime=1;
 	public static final int isfulltime=2;
-	public static final int emprateperhrs=20;
+	public static final int emprateperhour=20;
 	public static final int numofworkingdays=2;
-	public static void CalwageforMonth()
+	public static final int maxhrsinmonth=10;
+	public static void CalwageTotalmonth()
 	{
 		int empHrs=0;
-	      int empwage=0;
-	      int totalEmpwage=0;
-	      for(int day=0;day<numofworkingdays;day++) {
-	    	  int empCheck=(int) Math.floor(Math.random()*10)%3;
-	      switch (empCheck) {
-	      case isparttime:
-	        empHrs=4;
-	        break;
-	      case isfulltime:
-	    	  empHrs=8;
-	    	  break;
-	    	  default:
-	    		  empHrs=0;
-	      }
-	      empwage= empHrs*emprateperhrs;
-	      totalEmpwage+=empwage;
-	      System.out.println("empwage"+empwage);
-	      }
-	      System.out.println("total emp wage"+totalEmpwage);
+		int totalempHrs=0;
+		int totalworkingdays=0;
+		
+		while(totalempHrs<=maxhrsinmonth && totalworkingdays <numofworkingdays) {
+			totalworkingdays++;
+			int empCheck=(int) Math.floor(Math.random()*10)%3;
+			switch(empCheck) {
+			case isparttime:
+				empHrs=4;
+				break;
+			case isfulltime:
+				empHrs=8;
+				break;
+				default:
+					empHrs=0;
+			}
+			totalempHrs+=empHrs;
+			System.out.println("day"+ totalworkingdays+"emp hr"+empHrs);
+		
+		}
+		int totalEmpWage=totalempHrs*emprateperhour;
+		System.out.println("total empwage"+totalEmpWage);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		CalwageforMonth();
+		CalwageTotalmonth();
 }
 }
