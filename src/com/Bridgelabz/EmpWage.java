46efa27bbd88d2d1bfae0f3f1f76f78996cdaf9a
Divakar <divakar39312@gmail.com>
2022-08-09 21:40:35 +0530

    [Divakar]:UC3 added

--- src/com/Bridgelabz/EmpWage.java
+++ src/com/Bridgelabz/EmpWage.java
@@ -1,25 +1,22 @@
 package com.Bridgelabz;
 
 public class EmpWage {
-
-	public static void EmpDailywage()
+	public static void Parttimewage()
 	{
-		int isparttime=1;
-		int isfulltime=2;
-		int emprateperhr=20;
-		int empHr=0;
+		int isfulltime=1;
+		int empRatePerHour=20;
+		int empHrs=0;
 		int empwage=0;
-		double empCheck=Math.floor(Math.random()*10)%3;
-	   if(empCheck==isparttime)
-		   empHr=4;
-	   else if(empCheck==isfulltime)
-		   empHr=8;
-	   else
-		   empHr=0;
-	   empwage=empHr*emprateperhr;
-	   System.out.println("empWage"+empwage);
+		double empcheck=Math.floor(Math.random()*10)%2;
+		if(empcheck==isfulltime)
+			empHrs=8;
+		else
+			empHrs=0;
+		empwage=empHrs*empRatePerHour;
+		System.out.println("Emp wage"+empwage);
 	}
+
 	public static void main(String[] args) {
-		EmpDailywage();
-    }
+		Parttimewage();
+}
 }
