42b1c90fabd756725f31fd84700b0c5fd0992354
Divakar <divakar39312@gmail.com>
2022-08-09 21:51:59 +0530

    [Divakar]:UC5 added

--- src/com/Bridgelabz/EmpWage.java
+++ src/com/Bridgelabz/EmpWage.java
@@ -3,27 +3,34 @@ package com.Bridgelabz;
 public class EmpWage {
 	public static final int isparttime=1;
 	public static final int isfulltime=2;
-	public static final int Emprateperhr=20;
-	public static void UsingSwitchcase()
+	public static final int emprateperhrs=20;
+	public static final int numofworkingdays=2;
+	public static void CalwageforMonth()
 	{
 		int empHrs=0;
-	       int empwage=0;
-	       int empCheck=(int)Math.floor(Math.random()*10)%3;
-	       switch(empCheck) {
-	       case isparttime:
-	    	   empHrs=4;
-	    	   break;
-	       case isfulltime:
-	           empHrs=8;
-	           break;
-	         default:
-	        	 empHrs=0;
-	       }
-	       empwage=empHrs*Emprateperhr;
-	       System.out.println("empwage"+empwage);
+	      int empwage=0;
+	      int totalEmpwage=0;
+	      for(int day=0;day<numofworkingdays;day++) {
+	    	  int empCheck=(int) Math.floor(Math.random()*10)%3;
+	      switch (empCheck) {
+	      case isparttime:
+	        empHrs=4;
+	        break;
+	      case isfulltime:
+	    	  empHrs=8;
+	    	  break;
+	    	  default:
+	    		  empHrs=0;
+	      }
+	      empwage= empHrs*emprateperhrs;
+	      totalEmpwage+=empwage;
+	      System.out.println("empwage"+empwage);
+	      }
+	      System.out.println("total emp wage"+totalEmpwage);
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		UsingSwitchcase();
+		CalwageforMonth();
 }
 }
