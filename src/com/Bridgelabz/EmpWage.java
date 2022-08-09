537e9f6c0ed4586838f35342c35487a5db07b1db
Divakar <divakar39312@gmail.com>
2022-08-09 21:32:06 +0530

    [Divakar]:UC2Daily empwage added

--- src/com/Bridgelabz/EmpWage.java
+++ src/com/Bridgelabz/EmpWage.java
@@ -2,19 +2,24 @@ package com.Bridgelabz;
 
 public class EmpWage {
 
-	public static void EmpAttendce()
-    {
-    	int isfulltime=1;
-		double empCheck=Math.floor(Math.random()*10)%2;
-			if(empCheck==isfulltime) 
-				System.out.println("Employee is present");
-				else
-					System.out.println("employee is absent");
-    }
-
+	public static void EmpDailywage()
+	{
+		int isparttime=1;
+		int isfulltime=2;
+		int emprateperhr=20;
+		int empHr=0;
+		int empwage=0;
+		double empCheck=Math.floor(Math.random()*10)%3;
+	   if(empCheck==isparttime)
+		   empHr=4;
+	   else if(empCheck==isfulltime)
+		   empHr=8;
+	   else
+		   empHr=0;
+	   empwage=empHr*emprateperhr;
+	   System.out.println("empWage"+empwage);
+	}
 	public static void main(String[] args) {
-		System.out.println("welcome to Employee wage computation program");
-		
-		EmpAttendce();
+		EmpDailywage();
     }
 }
