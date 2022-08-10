c0e4457110e009b7ddcf90f3aacf2a854698c923
Divakar <divakar39312@gmail.com>
2022-08-10 14:45:56 +0530

    [Divakar]:UC7 added

--- src/com/Bridgelabz/EmpWage.java
+++ src/com/Bridgelabz/EmpWage.java
@@ -1,39 +1,61 @@
 package com.Bridgelabz;
 
 public class EmpWage {
-	public static final int isparttime=1;
-	public static final int isfulltime=2;
-	public static final int emprateperhour=20;
-	public static final int numofworkingdays=2;
-	public static final int maxhrsinmonth=10;
-	public static void CalwageTotalmonth()
-	{
-		int empHrs=0;
-		int totalempHrs=0;
-		int totalworkingdays=0;
+	//Constants
+		public static final int is_part_time = 1;
+		public static final int is_full_time = 2;
+		public static final int emp_rate_perhr = 10;
+		public static final int num_of_working_days = 20;
+		public static final int max_hrs_in_month = 100;
 		
-		while(totalempHrs<=maxhrsinmonth && totalworkingdays <numofworkingdays) {
-			totalworkingdays++;
-			int empCheck=(int) Math.floor(Math.random()*10)%3;
-			switch(empCheck) {
-			case isparttime:
-				empHrs=4;
-				break;
-			case isfulltime:
-				empHrs=8;
-				break;
-				default:
-					empHrs=0;
-			}
-			totalempHrs+=empHrs;
-			System.out.println("day"+ totalworkingdays+"emp hr"+empHrs);
+		//Class variables
+		//Variables 
+		 int totalWorkingDays = 0;
+		 int totalEmpHrs = 0;
+		 int totalEmpWage=0;
 		
+		public int Compute_EmployeeWage()
+		{
+			while(totalEmpHrs <= max_hrs_in_month && totalWorkingDays <= num_of_working_days)
+			 {	
+			 int empHrs = 0;
+			 int empWage = 0;
+				
+			totalWorkingDays++;
+		int emp_Check = (int) Math.floor ( Math.random() * 10 ) % 3;
+				
+			switch(emp_Check)
+				 {
+				case is_full_time:
+				 		empHrs = 8;
+				 		break;
+				 		
+		 	case is_part_time: 
+				 		empHrs = 4;
+				 		break;
+				 		
+		 	default:
+				 		empHrs = 0; 
+				 		break;
+				 	 
+				 }
+				 
+				 totalEmpHrs = empHrs;
+				 empWage=empHrs * emp_rate_perhr;
+				 totalEmpWage += empWage;
+				 System.out.println("Employee Wage =" +empWage);	 
+			 
+			 }
+			
+			System.out.println("\nTotal Employee Hours =" +totalEmpHrs);
+			
+			return totalEmpWage;
 		}
-		int totalEmpWage=totalempHrs*emprateperhour;
-		System.out.println("total empwage"+totalEmpWage);
-	}
-
-	public static void main(String[] args) {
-		CalwageTotalmonth();
-}
+				
+		public static void main(String args[])
+		{
+			EmpWage  Emp = new EmpWage();
+			
+			int result = Emp.Compute_EmployeeWage();
+			System.out.println("Total Employee Wage =" +result);}
 }
